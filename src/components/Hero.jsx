ec6cd1c422796796f8608bebcae4fa4951b652a7
swadhi03 <swathiksanthosh03@gmail.com>
2024-09-02 00:03:11 +0530

    completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                 <p>Tecverso - 2024</p>
                 <p>“The best way to predict the future is to create it.” — Peter Drucker</p>
                 <p>Join us as we explore the latest trends in technology and entrepreneurship, with interactive workshops, insightful talks from industry leaders, and competitions that challenge you to think creatively and critically.</p>
-                <a href="#"><button class="button">Register</button></a>
+                <a href="https://forms.gle/584tXQ1iooEgJC1f8"><button class="button">Register</button></a>
             </div>
         </div>
     )
